4a0995c0d40699f3fe8369b14fcdce35e2dd338d
junioralive <thejunioralive@gmail.com>
2023-12-24 17:33:18 +0530

    iPhone vlc fixed

--- spy_cli/util/scrapper.py
+++ spy_cli/util/scrapper.py
@@ -209,6 +209,7 @@ def ply(video_url, subtitle_url=None, platform='windows'):
         elif platform == 'iphone':
             # iPhone VLC command line might not support external subtitles
             format_video_url = video_url.replace(".mp4","")
+            print(format_video_url)
             print(f"\033]8;;vlc://{format_video_url}\033\\-------------------------\n- Tap to open -\n-------------------------\033]8;;\033\\\n")
             input("Press Enter to continue...")
 
